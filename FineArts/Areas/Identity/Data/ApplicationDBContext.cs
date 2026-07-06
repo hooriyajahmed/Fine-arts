34bef2f8dd588066732bf13effb2929523b887eb
Hooriya <jahmed5404@gmail.com>
2026-07-05 23:02:42 -0800

    identity initialize

---
+++ FineArts/Areas/Identity/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using FineArts.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FineArts.Areas.Identity.Data;
+
+public class ApplicationDBContext : IdentityDbContext<user>
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
