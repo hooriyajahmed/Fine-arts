39b10da205ff6db5c0d7bf2fbf2a8a1bb3fc6067
Hooriya <jahmed5404@gmail.com>
2026-07-21 21:15:59 -0800

    student crud

--- FineArts/Controllers/AdminController.cs
+++ FineArts/Controllers/AdminController.cs
@@ -1,12 +1,38 @@
+<<<<<<< Updated upstream
 ﻿using Microsoft.AspNetCore.Mvc;
+=======
+﻿using FineArts.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+>>>>>>> Stashed changes
 
 namespace FineArts.Controllers
 {
     public class AdminController : Controller
     {
+<<<<<<< Updated upstream
         public IActionResult Index()
         {
             return View();
         }
     }
 }
+=======
+        private readonly ApplicationDBContext dbcontext;
+
+        public AdminController(ApplicationDBContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult allcompetitions()
+        {
+            var data = dbcontext.compettions.ToList();
+            return View(data);
+        }
+    }
+}
+>>>>>>> Stashed changes
--- FineArts/obj/Debug/net8.0/FineArts.AssemblyInfo.cs
+++ FineArts/obj/Debug/net8.0/FineArts.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FineArts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337e62b96c93890d9223e510e34f9a0579c6ed56")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a12225183c86cec481c07ee007e30a47d0e01b")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("FineArts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FineArts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
